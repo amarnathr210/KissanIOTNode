feb0ff15fcb83871632cf4376913ee46c8325c59
amarnathr210 <amarnathr210@gmail.com>
2019-12-18 23:35:48 +0530

    commiting the mqtt changes

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,8 @@
 module.exports = {
   db: process.env.MONGODB_URL || "mongodb://localhost/kissaniot",
-  jwt_secret: process.env.jwt_secret
+  jwt_secret: process.env.jwt_secret,
+  CLOUDAMQP_MQTT_URL: process.env.CLOUDAMQP_MQTT_URL,
+  CLOUDAMQP_MQTT_ClientID: process.env.CLOUDAMQP_MQTT_ClientID,
+  CLOUDAMQP_MQTT_UserName: process.env.CLOUDAMQP_MQTT_UserName,
+  CLOUDAMQP_MQTT_Password: process.env.CLOUDAMQP_MQTT_Password
 };
